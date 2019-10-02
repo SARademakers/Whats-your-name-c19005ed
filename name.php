79c28ab361f85b4280416b2301115a0c2f740aac
SARademakers <2049137@talnet.nl>
2019-10-02 11:27:42 +0200

    Uitwerking van de exercise

--- name.php
+++ name.php
@@ -3,4 +3,4 @@ echo ("wat is je voornaam?" . PHP_EOL);
 $line1 = readline();
 echo ("wat is je achternaam?" . PHP_EOL);
 $line2 = readline();
-print ("Jouw naam is:" . $line1 . $line2)
+print ("Jouw naam is:" . $line1 . $line2);
